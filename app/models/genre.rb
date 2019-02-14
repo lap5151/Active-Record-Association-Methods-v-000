0c0ea0c815c183f4ce0320526b6cb9854370d9fe
Lauren Pardee <lap5151@gmail.com>
2019-02-14 14:25:20 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,7 +11,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artist_anmes = []
+    artist_names = []
     self.artists.each do |artist|
       artist_names << artist.name
     end
